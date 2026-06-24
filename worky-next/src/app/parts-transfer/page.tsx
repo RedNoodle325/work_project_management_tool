a0ca1b5f120496181c49c6c669c38d1532d6fc13
Zak Klinedinst <111690550+RedNoodle325@users.noreply.github.com>
2026-06-24 16:12:26 -0400

    Delete worky-next/src/app/parts-transfer directory

--- worky-next/src/app/parts-transfer/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Suspense } from 'react'
-import { AuthGuard } from '@/components/AuthGuard'
-import { PartsTransfer } from '@/pages-impl/PartsTransfer'
-export default function PartsTransferPage() {
-  return <AuthGuard><Suspense><PartsTransfer /></Suspense></AuthGuard>
-}
