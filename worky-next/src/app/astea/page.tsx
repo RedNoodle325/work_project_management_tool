27900e7a1888316ba7af890647ac2946876dec3d
Zak Klinedinst <111690550+RedNoodle325@users.noreply.github.com>
2026-06-24 16:11:50 -0400

    Delete worky-next/src/app/astea directory

--- worky-next/src/app/astea/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AuthGuard } from '@/components/AuthGuard'
-import { AsteaWizard } from '@/pages-impl/AsteaWizard'
-
-export default function AsteaPage() {
-  return <AuthGuard><AsteaWizard /></AuthGuard>
-}
